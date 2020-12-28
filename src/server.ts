4f84c2b736dc3e470c6df7e3ffc1b1fe17fd64a2
ThaSMorato <tha.s.morato@gmail.com>
2020-12-28 14:59:13 -0300

    env port

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import * as express from 'express';
 import routes from './routes';
 import * as cors from 'cors';
+require('dotenv/config');
 
 const app = express();
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(port);
